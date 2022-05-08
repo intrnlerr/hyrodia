a762bfc12cdb016b13cd08760aab36f8f9a3d6dd
intrnlerr <nulltestb1@gmail.com>
2022-05-08 05:26:32 +0000

    add World class

--- src/main/kotlin/world.kt
+++ src/main/kotlin/world.kt
@@ -1,11 +1,7 @@
 import kotlin.random.Random
 
 enum class ResourceType {
-    Sheep,
-    Lumber,
-    Brick,
-    Ore,
-    Grain
+    Sheep, Lumber, Brick, Ore, Grain
 }
 
 class Tile(val resource_type: ResourceType, val roll: Int)
@@ -15,4 +11,15 @@ fun getTile(x: Int, y: Int): Tile {
     val roll = intArrayOf(2, 3, 4, 5, 6, 8, 9, 10, 11, 12).random(random)
     val resource = ResourceType.values().random(random)
     return Tile(resource, roll)
+}
+
+class Player
+class HexPosition(x: Int, y: Int)
+class Town(val owner: Player, val is_city: Boolean)
+
+class World(
+    val roads: Map<HexPosition, Player>, val towns: Map<HexPosition, Town>
+) {
+    constructor() : this(HashMap(), HashMap())
+
 }
