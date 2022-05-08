2a5a5508066110e9e7940963118cad5715a924e4
intrnlerr <nulltestb1@gmail.com>
2022-05-08 20:45:19 +0000

    coordinates

--- src/main/kotlin/world.kt
+++ src/main/kotlin/world.kt
@@ -14,12 +14,53 @@ fun getTile(x: Int, y: Int): Tile {
 }
 
 class Player
-class HexPosition(x: Int, y: Int)
+data class HexPosition(val x: Int, val y: Int) {
+    fun northWest() = HexPosition(x - 1, y + 1)
+    fun northEast() = HexPosition(x, y + 1)
+    fun southWest() = HexPosition(x, y - 1)
+    fun southEast() = HexPosition(x + 1, y - 1)
+    fun east() = HexPosition(x + 1, y)
+    fun west() = HexPosition(x - 1, y)
+}
+
+enum class VertexType {
+    Top, Bottom,
+}
+
+data class VertexPosition(val position: HexPosition, val vertexType: VertexType) {
+    fun hexNeighbors(): Array<HexPosition> {
+        return when (vertexType) {
+            VertexType.Top -> arrayOf(position, position.northEast(), position.northWest())
+            VertexType.Bottom -> arrayOf(position, position.southEast(), position.southWest())
+        }
+    }
+}
+
+enum class EdgeType {
+    West, NorthWest, NorthEast
+}
+
+data class EdgePosition(val position: HexPosition, val edgeType: EdgeType) {
+    fun vertexNeighbors(): Array<VertexPosition> {
+        return when (edgeType) {
+            EdgeType.West -> arrayOf(
+                VertexPosition(position.northWest(), VertexType.Bottom),
+                VertexPosition(position.southWest(), VertexType.Top)
+            )
+            EdgeType.NorthWest -> arrayOf(
+                VertexPosition(position, VertexType.Top), VertexPosition(position.northWest(), VertexType.Bottom)
+            )
+            EdgeType.NorthEast -> arrayOf(
+                VertexPosition(position, VertexType.Top), VertexPosition(position.northEast(), VertexType.Bottom)
+            )
+        }
+    }
+}
+
 class Town(val owner: Player, val is_city: Boolean)
 
-class World(
-    val roads: Map<HexPosition, Player>, val towns: Map<HexPosition, Town>
-) {
-    constructor() : this(HashMap(), HashMap())
+class World {
+    val roads: Map<EdgePosition, Player> = HashMap()
+    val towns: Map<VertexPosition, Town> = HashMap()
 
 }
